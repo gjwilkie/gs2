656a3af37bf043915c91ff1a0d0858cb93e92325
tatsuno <tatsuno@28a56f14-469b-1442-acfb-e68ebda8abd8>
2009-01-24 22:23:29 +0000

    Added capability of C function pointer for index resolution. Things to be done:  - gs2_layouts_indices.c and gs2_layouts_type.f90 may be combined with AstroGK  - makehead.awk should go to utils/  - Makefile may be simplified for duplicate description  - more functions for xxf & yxf layouts --- if needed

---
+++ gs2/trunk/gs2_layouts_type.f90
@@ -0,0 +1,32 @@
+module gs2_layouts_type
+  ! This can be made by just replacing nakx by ntheta0 from agk_layouts_type.f90
+
+  implicit none
+
+  type :: g_layout_type
+     integer :: iproc
+     integer :: naky, ntheta0, nlambda, negrid, nspec
+     integer :: llim_world, ulim_world, llim_proc, ulim_proc, ulim_alloc, blocksize
+  end type g_layout_type
+
+  type :: lz_layout_type
+     integer :: iproc
+     integer :: ntgrid, naky, ntheta0, negrid, nspec, ng2
+     integer :: llim_world, ulim_world, llim_proc, ulim_proc, ulim_alloc, blocksize, gsize
+     integer :: llim_group, ulim_group, igroup, ngroup, nprocset, iset, nset, groupblocksize
+  end type lz_layout_type
+
+  type :: e_layout_type
+     integer :: iproc
+     integer :: ntgrid, naky, ntheta0, nlambda, nspec, nsign
+     integer :: llim_world, ulim_world, llim_proc, ulim_proc, ulim_alloc, blocksize
+  end type e_layout_type
+
+  type :: le_layout_type
+     sequence
+     integer :: iproc
+     integer :: ntgrid, naky, ntheta0, nspec
+     integer :: llim_world, ulim_world, llim_proc, ulim_proc, ulim_alloc, blocksize
+  end type le_layout_type
+
+end module gs2_layouts_type
