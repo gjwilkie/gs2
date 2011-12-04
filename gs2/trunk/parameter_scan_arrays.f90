4beac00e1c16d1f55da03d9e375985a21974dc1b
edmundhighcock <edmundhighcock@28a56f14-469b-1442-acfb-e68ebda8abd8>
2011-12-04 21:11:32 +0000

    --Added the ability to restart parameter scans

--- gs2/trunk/parameter_scan_arrays.f90
+++ gs2/trunk/parameter_scan_arrays.f90
@@ -8,6 +8,21 @@ module parameter_scan_arrays
   public :: write_scan_parameter
   logical :: write_scan_parameter
 
+  public :: run_scan
+  logical :: run_scan
+
+  integer :: scan_parameter_switch
+  public :: scan_parameter_switch
+
+  integer, parameter :: scan_parameter_tprim = 1, &
+                        scan_parameter_g_exb = 2
+
+  public :: scan_parameter_tprim, &
+            scan_parameter_g_exb
+
+  integer :: scan_spec
+  public :: scan_spec
+
   public :: hflux_tot, momflux_tot, phi2_tot
   real, dimension(:), allocatable :: hflux_tot, momflux_tot, phi2_tot
 
