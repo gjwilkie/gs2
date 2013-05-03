38581043fd109c016e42f26e065ece058dd810da
ddickinson <ddickinson@28a56f14-469b-1442-acfb-e68ebda8abd8>
2013-05-03 14:33:54 +0000

    Adding ability to only enforce the parity of a specified ik mode. This is used by a new type of 'secondary' instability analysis also added in this commit. By setting fixpar_secondary (in knobs) to an integer between 1 and naky one can filter out either the even or odd component of that specific ik, whilst adding in another fixed term. This is designed to be used with ginit_option='fixpar' only.

--- init_g.f90
+++ init_g.f90
@@ -27,7 +27,7 @@ module init_g
        ginitopt_nl3r = 26, ginitopt_smallflat = 27, ginitopt_harris = 28, &
        ginitopt_recon3 = 29, ginitopt_ot = 30, &
        ginitopt_zonal_only = 31, ginitopt_single_parallel_mode = 32, &
-       ginitopt_all_modes_equal = 33
+       ginitopt_all_modes_equal = 33, ginitopt_fixpar= 34
 
   real :: width0, dphiinit, phiinit, imfac, refac, zf_init, phifrac
   real :: den0, upar0, tpar0, tperp0
@@ -248,13 +248,18 @@ contains
           write (unit, fmt="(' tperp1 = ',e16.10)") tperp1
           write (unit, fmt="(' tperp2 = ',e16.10)") tperp2
 
+
+       case (ginitopt_fixpar)
+          write (unit, fmt="(' ginit_option = ',a)") '"fixpar"'
+          write (unit, fmt="(' scale = ',e16.10)") scale
+
        end select
        write (unit, fmt="(' /')")
   end subroutine wnml_init_g
 
  
   subroutine check_init_g(report_unit)
-  use run_parameters, only : delt_option_switch, delt_option_auto
+  use run_parameters, only : delt_option_switch, delt_option_auto, fixpar_secondary
   use species, only : spec, has_electron_species
   implicit none
   integer :: report_unit
@@ -542,6 +547,24 @@ contains
        write (report_unit, fmt="('################# WARNING #######################')")
        write (report_unit, *) 
 
+    case (ginitopt_fixpar)
+       write (report_unit, fmt="('Initial conditions:')")
+       write (report_unit, *) 
+       write (report_unit, fmt="('################# WARNING #######################')")
+       write (report_unit, fmt="('Under development for study of fixed parity+amplitude primary on secondary.')")
+       write (report_unit, fmt="('Fixing ik:   ',i8)") fixpar_secondary
+       write (report_unit, fmt="('################# WARNING #######################')")
+       write (report_unit, *) 
+
+       if(fixpar_secondary.le.0)then
+          write (report_unit, *) 
+          write (report_unit, fmt="('################# WARNING #######################')")
+          write (report_unit, fmt="('ginit_option = fixpar requires fixpar_secondary>0.')") 
+          write (report_unit, fmt="('fixpar_secondary= ',i8)") fixpar_secondary
+          write (report_unit, fmt="('THIS IS AN ERROR.')") 
+          write (report_unit, fmt="('################# WARNING #######################')")
+          write (report_unit, *) 
+       endif
     end select
 
     if (ginitopt_switch == ginitopt_restart_many) then
@@ -781,6 +804,8 @@ contains
        call ginit_recon3
     case (ginitopt_ot)
        call ginit_ot
+    case (ginitopt_fixpar)
+       call ginit_fixpar
     end select
   end subroutine ginit
 
@@ -791,7 +816,7 @@ contains
 
     implicit none
 
-    type (text_option), dimension (32), parameter :: ginitopts = &
+    type (text_option), dimension (33), parameter :: ginitopts = &
          (/ text_option('default', ginitopt_default), &
             text_option('noise', ginitopt_noise), &
             text_option('xi', ginitopt_xi), &
@@ -823,7 +848,8 @@ contains
             text_option('ot', ginitopt_ot), &
             text_option('zonal_only', ginitopt_zonal_only), &
             text_option('single_parallel_mode', ginitopt_single_parallel_mode), &
-            text_option('all_modes_equal', ginitopt_all_modes_equal) &
+            text_option('all_modes_equal', ginitopt_all_modes_equal), &
+            text_option('fixpar',ginitopt_fixpar) &
             /)
     character(20) :: ginit_option
     namelist /init_g_knobs/ ginit_option, width0, phiinit, chop_side, &
@@ -3639,6 +3665,67 @@ contains
 
   end subroutine ginit_restart_zonal_only
 
+  subroutine ginit_fixpar
+    use mp, only: proc0,mp_abort
+    use gs2_save, only: gs2_restore
+    use kt_grids, only: naky, ntheta0
+    use dist_fn, only : parity_redist_ik, init_enforce_parity
+    use dist_fn_arrays, only: g, gnew, g_fixpar
+    use fields_arrays, only: phi, apar, bpar
+    use fields_arrays, only: phinew, aparnew, bparnew
+    use gs2_layouts, only: g_lo, ik_idx
+    use file_utils, only: error_unit
+    use run_parameters, only: fphi, fapar, fbpar
+    use run_parameters, only : fixpar_secondary
+    implicit none
+    integer :: iglo, istatus
+    integer :: ig, ik, it, ierr
+
+    !Check we should be using this routine
+    if(fixpar_secondary.le.0)then
+       ierr=error_unit()
+       if(proc0) write(ierr,'("ERROR: ginit_option=fixpar should only be used with fixpar_secondary>0")')
+       call mp_abort("Aborting due to incompatible options.")
+    endif
+
+    !Restore the restart data
+    call gs2_restore (g, scale, istatus, fphi, fapar, fbpar)
+    if (istatus /= 0) then
+       ierr = error_unit()
+       if (proc0) write(ierr,*) "Error reading file: ", trim(restart_file)
+       g = 0.
+    end if
+
+    !Set all distribution function elements to zero except for 
+    !ik=fixpar_secondary
+    do iglo = g_lo%llim_proc, g_lo%ulim_proc
+       if (ik_idx(g_lo,iglo).eq.fixpar_secondary) cycle
+       g (:,1,iglo) = 0.
+       g (:,2,iglo) = 0.
+    end do
+
+    !Set all the field components to zero except where required
+    do ik = 1, naky
+       do it=1,ntheta0
+          if (ik_idx(g_lo,iglo).eq.fixpar_secondary) cycle
+          phinew(:,it,ik) = 0.
+          aparnew(:,it,ik) = 0.
+          bparnew(:,it,ik) = 0.
+          phi(:,it,ik) = 0.
+          apar(:,it,ik) = 0.
+          bpar(:,it,ik) = 0.          
+       end do
+    end do
+    
+    !Copy data into gnew
+    gnew = g
+
+    !Populate array
+    g_fixpar=gnew
+
+    !Initialise parity redist object
+    call init_enforce_parity(parity_redist_ik,fixpar_secondary)
+  end subroutine ginit_fixpar
 
 
 
