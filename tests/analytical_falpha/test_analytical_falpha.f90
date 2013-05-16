169dd3d9115de38b735dce607f01dee973e259a1
edmundhighcock <edmundhighcock@28a56f14-469b-1442-acfb-e68ebda8abd8>
2013-05-16 18:20:40 +0000

    --falpha_integrand tested and working --falpha test failing

--- tests/analytical_falpha/test_analytical_falpha.f90
+++ tests/analytical_falpha/test_analytical_falpha.f90
@@ -1,9 +1,21 @@
 
+!> A program that runs unit tests on the analytical_falpha module.
+!! The test results were calculated using sage and are viewable at
+!! http://www.sagenb.org/home/pub/5036
+!!
+!! This is free software released under GPLv3
+!!   Written by: Edmund Highcock (edmundhighcock@sourceforge.net)
 program test_analytical_falpha
+  use unit_tests
   use analytical_falpha
-  real :: energy, rslt
-
+  implicit none
+  real :: eps
   type(analytical_falpha_parameters_type) :: parameters
+
+  ! General config
+  eps = 1.0e-8
+
+
   write (*,*) '*************************'
   write (*,*) 'Testing analytical_falpha'
   write (*,*) '*************************'
@@ -13,36 +25,35 @@ program test_analytical_falpha
   call announce_test('chandrasekhar')
   call process_test(analytical_falpha_unit_test_chandrasekhar(), 'chandrasekhar')
 
-  parameters%alpha_ion_collision_rate = 0.01
-  parameters%alpha_electron_collision_rate = 0.0006
+  parameters%alpha_ion_collision_rate = 0.1/4.0**2.0
+  parameters%alpha_electron_collision_rate = &
+     parameters%alpha_ion_collision_rate / 1836.0**2.0
   parameters%ion_vth = 1.0
   parameters%electron_vth = 42.0
   parameters%alpha_vth = (3.6e2 / 4.0)**0.5
+  parameters%ion_temp = 1.0
+  parameters%alpha_mass = 4.0
 
   call announce_test('nu_parallel')
-  energy = 0.8
-  rslt = 6.9741586014337e-5
-  call process_test(analytical_falpha_unit_test_nu_parallel(parameters, energy, &
-    rslt), 'nu_parallel')
+  call process_test(analytical_falpha_unit_test_nu_parallel( &
+    parameters, 0.8, 0.000121314067605073, eps), 'nu_parallel')
+
+  
+  call announce_test('falpha_integrand')
+  call process_test(analytical_falpha_unit_test_falpha_integrand(&
+    parameters, 0.4,  0.9, 74269.3906980909, eps), 'falpha_integrand 0.9')
+  call process_test(analytical_falpha_unit_test_falpha_integrand(&
+    parameters, 0.4,  0.01, 3428.53572237518, eps), 'falpha_integrand 0.01')
+  call process_test(analytical_falpha_unit_test_falpha_integrand(&
+    parameters, 0.4,  2.5, 3.03673247901918e6, eps), 'falpha_integrand 2.5')
+
+  call announce_test('falpha')
+  call process_test(analytical_falpha_unit_test_falpha(&
+    parameters, 0.02, 0.01, 16, 63.562943357601526, eps), 'falpha 0.02') 
 
 
 
 
-contains
-  subroutine announce_test(test_name)
-    character(*), intent(in) :: test_name
-    write (*,*) '--> Testing ', test_name
-  end subroutine announce_test
-  subroutine process_test(rslt, test_name)
-    logical, intent (in) :: rslt
-    character(*), intent(in) :: test_name
-    if (.not. rslt) then 
-      write(*,*) '--> ', test_name, ' failed'
-      stop 1
-    end if
-
-    write (*,*) '--> ', test_name, ' passed'
-  end subroutine process_test
 
 
 end program test_analytical_falpha
