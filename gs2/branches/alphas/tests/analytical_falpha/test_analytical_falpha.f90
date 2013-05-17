85740a5eb5072a910181b4fb90d9f1e38b8b18fa
edmundhighcock <edmundhighcock@28a56f14-469b-1442-acfb-e68ebda8abd8>
2013-05-17 18:05:20 +0000

    --dfalphade still not working

--- gs2/branches/alphas/tests/analytical_falpha/test_analytical_falpha.f90
+++ gs2/branches/alphas/tests/analytical_falpha/test_analytical_falpha.f90
@@ -36,6 +36,7 @@ program test_analytical_falpha
   parameters%alpha_mass = 4.0
   parameters%source = 0.01
   parameters%alpha_injection_energy = 3.6e2
+  parameters%energy_0 = 0.01
 
   call announce_test('nu_parallel')
   call process_test(analytical_falpha_unit_test_nu_parallel( &
@@ -52,30 +53,29 @@ program test_analytical_falpha
 
   call announce_test('falpha')
   call process_test(analytical_falpha_unit_test_falpha(&
-    parameters, 0.02, 0.01, 128, 264.9390892941125*0.01, eps), 'falpha 0.02') 
+    parameters, 0.02, 0.01, 128, 264.9390892941125*0.01 / 4.0 / 3.14159265358979, eps), 'falpha 0.02') 
   call process_test(analytical_falpha_unit_test_falpha(&
-    parameters, 0.5, 0.01, 256, 4571.551171450769*0.01, eps), 'falpha 0.5') 
+    parameters, 0.5, 0.01, 256, 4571.551171450769*0.01 / 4.0 / 3.14159265358979, eps), 'falpha 0.5') 
   call process_test(analytical_falpha_unit_test_falpha(&
-    parameters, 1.0, 0.01, 256, 6233.062047471938*0.01, eps), 'falpha 1.0') 
+    parameters, 1.0, 0.01, 256, 6233.062047471938*0.01 / 4.0 / 3.14159265358979, eps), 'falpha 1.0') 
   call process_test(analytical_falpha_unit_test_falpha(&
-    parameters, 2.5, 0.01, 256, 310.32588629862335*0.01, eps), 'falpha 2.5') 
+    parameters, 2.5, 0.01, 256, 310.32588629862335*0.01 / 4.0 / 3.14159265358979, eps), 'falpha 2.5') 
   call process_test(analytical_falpha_unit_test_falpha(&
     parameters, 0.01, 0.01, 128, 0.0, eps), 'falpha 0.01') 
 
 
   allocate(array(6, 1,  7))
 
-  array(:,1,1) = (/0.0100000000000000, 0.675000000000000, 1.34000000000000, &
-  2.00500000000000, 2.67000000000000, 3.33500000000000/) ! egrid
-  array(:,1,5) = (/0.000000000000000, 53.4776774832068, 31.5777514771834, &
-  8.35159722869193, 2.20880756252364, 0.584179374874592/) ! f0_rslt
+  array(:,1,1) = (/0.0200000000000000, 0.683333333333333, 1.34666666666667, 2.01000000000000, 2.67333333333333, 3.33666666666667/) ! egrid
+  array(:,1,5) = (/2.64939089294112, 53.7838275060886, 31.1595092683755, 8.26849744780646, 2.19413115449062, 0.582235352129606/)  / 4.0 / 3.14159265358979! f0_rslt
+  array(:,1,6) = (/76.1206676344528, 0.677300806262160, -1.99999999999978, -2.00000000000422, -2.00000000000422, -2.00000000000644/) ! f0_rslt
 
   parameters%negrid = 6
   parameters%alpha_is = 1
   call announce_test('calculate_arrays')
   call process_test(analytical_falpha_unit_test_calculate_arrays(&
     parameters, array(:,:,1), array(:,:,2), array(:,:,3), array(:,:,4),&
-    array(:,:,5), eps), 'calculate_arrays') 
+    array(:,:,5), array(:,:,6), eps), 'calculate_arrays') 
 
 
 
