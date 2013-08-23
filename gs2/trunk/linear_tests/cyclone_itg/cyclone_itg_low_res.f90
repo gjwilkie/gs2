8424df3fac76f96801511182f6936fe41bfaf56e
edmundhighcock <edmundhighcock@28a56f14-469b-1442-acfb-e68ebda8abd8>
2013-08-23 17:37:14 +0000

    --New system for running tests on HPC systems

--- gs2/trunk/linear_tests/cyclone_itg/cyclone_itg_low_res.f90
+++ gs2/trunk/linear_tests/cyclone_itg/cyclone_itg_low_res.f90
@@ -1,17 +1,23 @@
 !> A test program to run a linear cyclone itg benchmark
 !! at low resolution and check the growth rate
 
-program cyclone_itg_low_res
+module checks_mod
   use functional_tests
-
-
-  call test_gs2('Linear cyclone base case ITG in single mode', checks)
-
+public checks
 contains
   function checks()
     logical :: checks
     ! We only expect this low res test to get the growth rate to 1%
     checks =  check_growth_rate((/0.1687/), 0.01)
   end function checks
+end module checks_mod
+
+program cyclone_itg_low_res
+  use functional_tests
+  use checks_mod
+
+
+  call test_gs2('Linear cyclone base case ITG in single mode', checks)
+
 
 end program cyclone_itg_low_res
