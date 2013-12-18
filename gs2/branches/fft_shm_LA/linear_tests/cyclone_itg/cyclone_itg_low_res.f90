eb0cc5e7ca14f210ed67556f743010b91584dae8
antonl22 <antonl22@28a56f14-469b-1442-acfb-e68ebda8abd8>
2013-12-18 18:10:11 +0000

    shared memory tests on fft

---
+++ gs2/branches/fft_shm_LA/linear_tests/cyclone_itg/cyclone_itg_low_res.f90
@@ -0,0 +1,23 @@
+!> A test program to run a linear cyclone itg benchmark
+!! at low resolution and check the growth rate
+
+module checks_mod
+  use functional_tests
+  public checks
+  contains
+    function checks()
+      logical :: checks
+      ! We only expect this low res test to get the growth rate to 3%
+      checks =  check_growth_rate((/0.1687/), 0.03)
+    end function checks
+end module checks_mod
+
+program cyclone_itg_low_res
+  use functional_tests
+  use checks_mod
+
+
+  call test_gs2('Linear cyclone base case ITG in single mode', checks)
+
+
+end program cyclone_itg_low_res
