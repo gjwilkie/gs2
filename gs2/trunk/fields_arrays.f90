5a7fa5df4a6d8bd439c88c7e3e1f04119eaa0b44
ddickinson <ddickinson@28a56f14-469b-1442-acfb-e68ebda8abd8>
2014-09-18 15:32:46 +0000

    Minor : Small tidying up of fields+fields_implicit+fields_arrays, main changes are to move fields_implicit specific objects from fields_arrays into fields_implicit and to add zeroing of apar+bpar to reset_init.

--- gs2/trunk/fields_arrays.f90
+++ gs2/trunk/fields_arrays.f90
@@ -8,27 +8,6 @@ module fields_arrays
   complex, dimension (:,:,:), allocatable :: apar_ext
   ! (-ntgrid:ntgrid,ntheta0,naky) replicated
 
-  integer, save :: nidx
-
+  !Timing data
   real, save :: time_field(2)=0.
-
-! Within each supercell, there are are N_class primary cells.  Each 
-! has (2*ntgrid+1)*nfield points.
-  type dcell_type
-     complex, dimension(:), pointer :: supercell => null()
-  end type dcell_type
-
-! Within each class, there may be multiple supercells.
-
-! The number of supercells in each class is M_class.
-
-! When aminv is laid out over PEs, the supercells of each class 
-! are distributed -- thus, "dcells"
-
-  type :: field_matrix_type
-     type(dcell_type), dimension (:), pointer :: dcell => null()
-  end type field_matrix_type
-
-! There may be supercells of different sizes or "classes".  
-  type (field_matrix_type), dimension (:), allocatable :: aminv
 end module fields_arrays
