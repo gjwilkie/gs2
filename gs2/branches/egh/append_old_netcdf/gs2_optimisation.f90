a396c2e8c6a83cddf411e863a98242ddbf4d4b4c
edmundhighcock <edmundhighcock@28a56f14-469b-1442-acfb-e68ebda8abd8>
2015-03-16 11:42:03 +0000

    --Optimisation of layout working

--- gs2/branches/egh/append_old_netcdf/gs2_optimisation.f90
+++ gs2/branches/egh/append_old_netcdf/gs2_optimisation.f90
@@ -14,6 +14,9 @@ module gs2_optimisation
       call init_mp
       state%mp_comm_external = .true.
       state%mp_comm = mp_comm
+      allocate(state%optim%sorted_optimisations(0))
+      allocate(state%optim%sorted_results(0))
+      write (*,*) 'size', size(state%optim%sorted_results)
       ! We have to initialize_gs2 so that we 
       ! can read the optimisation_config namelist
       call initialize_gs2(state)
@@ -32,10 +35,12 @@ module gs2_optimisation
     subroutine optimise_gs2(state)
       type(gs2_program_state_type), intent(inout) :: state
 
+      state%print_times = .false.
+      state%is_external_job = .true.
       ! Initialize optimisation results
-      call measure_timestep(state)
-      state%optim%results%optimal_time = state%optim%results%last_time
-      state%optim%results%optimal = .true.
+      !call measure_timestep(state)
+      state%optim%results%optimal_time = -1.0
+      state%optim%results%optimal = .false.
 
       if (state%optim%max_unused_procs .eq. 0 &
           .or. state%optim%max_imbalance .eq. 0.0) then
@@ -44,7 +49,28 @@ module gs2_optimisation
         call optimise_nprocs(state)
       end if
 
+      call output_results(state)
+
     end subroutine optimise_gs2
+
+    subroutine output_results(state)
+      use mp, only: proc0
+      implicit none
+      type(gs2_program_state_type), intent(inout) :: state
+      integer :: i,n
+
+      if (proc0) then
+        write (state%optim%outunit, '(A16," ",A6)') 'timestep-time', 'layout' 
+        n = size(state%optim%sorted_results)
+        do i = 1,n
+            write(state%optim%outunit, &
+              '(E16.9," ",A6)') &
+            state%optim%sorted_results(i)%time, &
+            state%optim%sorted_optimisations(i)%layout
+        end do
+      end if
+
+    end subroutine output_results
 
     subroutine optimise_nprocs(state)
       type(gs2_program_state_type), intent(inout) :: state
@@ -58,12 +84,12 @@ module gs2_optimisation
       state%init%opt_ov%override_layout = .true.
       state%init%opt_ov%layout = 'lxyes'
       call optimise_flags(state)
-      if (proc0) write (*,*) 'layout', state%init%opt_ov%layout, &
-        'time', state%optim%results%last_time
       state%init%opt_ov%layout = 'lexys'
       call optimise_flags(state)
-      if (proc0) write (*,*) 'layout', state%init%opt_ov%layout, &
-        'time', state%optim%results%last_time
+      state%init%opt_ov%layout = 'xyles'
+      call optimise_flags(state)
+      state%init%opt_ov%layout = 'yxles'
+      call optimise_flags(state)
     end subroutine optimise_layout
 
     subroutine optimise_flags(state)
@@ -81,25 +107,67 @@ module gs2_optimisation
       use gs2_main, only: finalize_diagnostics
       use gs2_main, only: finalize_equations
       use gs2_main, only: finalize_gs2
+      use overrides, only: optimisations_overrides_type
+      use optimisation_config, only: optimisation_results_type
 
       implicit none
       type(gs2_program_state_type), intent(inout) :: state
+      type(optimisations_overrides_type), &
+        dimension(:), allocatable :: sorted_opts_temp
+      type(optimisation_results_type), &
+        dimension(:), allocatable :: sorted_res_temp
+      integer :: i,n
+      real :: t
 
       call initialize_gs2(state)
       call initialize_equations(state)
       call initialize_diagnostics(state)
-      call evolve_equations(state, 20)
+      call evolve_equations(state, 200)
       call finalize_diagnostics(state)
       call finalize_equations(state)
       call finalize_gs2(state)
 
-      state%optim%results%last_time = state%timers%advance(1)/10.0
-      if (state%optim%results%last_time .le. &
-          state%optim%results%optimal_time) then
-        state%optim%results%optimal_time = &
-        state%optim%results%last_time
+      t = state%timers%advance(1)/200.0
+      state%optim%results%time = t
+      if (t .lt.  state%optim%results%optimal_time .or. &
+        state%optim%results%optimal_time .lt. 0.0) then
+        state%optim%results%optimal_time = t
         state%optim%results%optimal = .true.
       end if
+
+      n = size(state%optim%sorted_results)
+      write (*,*) 'size2', size(state%optim%sorted_results)
+      allocate(sorted_opts_temp(n), sorted_res_temp(n))
+      if (n>0) then
+        do i = 1,n
+          sorted_opts_temp(i) = state%optim%sorted_optimisations(i)
+          sorted_res_temp(i) = state%optim%sorted_results(i)
+          end do
+      end if
+
+      deallocate(state%optim%sorted_optimisations)
+      deallocate(state%optim%sorted_results)
+      allocate(state%optim%sorted_optimisations(n+1))
+      allocate(state%optim%sorted_results(n+1))
+
+      i=1
+      do 
+        if (i>n) exit
+        if (sorted_res_temp(i)%time > t) exit
+        state%optim%sorted_optimisations(i) = sorted_opts_temp(i)
+        state%optim%sorted_results(i) = sorted_res_temp(i)
+        i = i+1
+      end do
+      state%optim%sorted_optimisations(i) = state%init%opt_ov
+      state%optim%sorted_results(i) = state%optim%results
+      i = i + 1
+      do while (i < n+2)
+        state%optim%sorted_optimisations(i) = sorted_opts_temp(i-1)
+        state%optim%sorted_results(i) = sorted_res_temp(i-1)
+        i = i + 1
+      end do
+        
+      deallocate(sorted_opts_temp, sorted_res_temp)
       
     end subroutine measure_timestep
 
