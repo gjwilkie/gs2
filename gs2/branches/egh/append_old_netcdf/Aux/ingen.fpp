a8e504c6b18c926bb7e0260644b946d8fa77dd3f
edmundhighcock <edmundhighcock@28a56f14-469b-1442-acfb-e68ebda8abd8>
2015-09-23 16:00:18 +0000

    --Deleted redundant no_autotools and append_old_netcdf branches

--- gs2/branches/egh/append_old_netcdf/Aux/ingen.fpp
+++
@@ -1,5 +0,0 @@
-
-program ingen
-  use ingen_mod, only: run_ingen
-  call run_ingen
-end program ingen
--- gs2/branches/egh/append_old_netcdf/tests/valgrind_tools/generate_suppressions.rb
+++
@@ -1,6 +0,0 @@
-#p ARGV;exit
-system("valgrind --gen-suppressions=all --error-limit=no #{ARGV.join(' ')} 2> .supptmpfile")
-text = File.read(".supptmpfile")
-text = text.gsub(/^==.*$/, '').scan(/\{.*?\}/m).uniq.join("\n")
-File.open('valgrind.supp', 'w'){|f| f.puts text}
-
